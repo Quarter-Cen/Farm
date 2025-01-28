7e4e28c0530595fcb6801daf4b64f637be49c962
TarKhachonsak <149598755+TarKhachonsak@users.noreply.github.com>
2025-01-28 21:46:13 +0700

    ฟังก์ชันหน้าข้อมูลวัว

---
+++ src/services/cowData/ICowData.ts
@@ -0,0 +1,44 @@
+import { Cow } from "@prisma/client";
+
+export interface ICowData {
+    getCowData(
+        id: bigint,
+        name: string,
+        gender: string,
+        age: number,
+        weight: number,
+        birthdate: Date,
+        breed: string,
+        healthStatus: string,
+        recordedAt: Date,
+        veterianId: bigint
+    ): Promise<Cow | null>;
+
+    editCowData(
+        id: bigint,
+        name?: string,
+        gender?: string,
+        age?: number,
+        weight?: number,
+        birthdate?: Date,
+        breed?: string,
+        healthStatus?: string,
+        recordedAt?: Date,
+    ): Promise<Cow | null>;
+
+    createCowData(
+        name: string,
+        gender: string,
+        age: number,
+        weight: number,
+        birthdate: Date,
+        breed: string,
+        healthStatus: string,
+        recordedAt: Date,
+        veterianId: bigint
+    ): Promise<Cow>
+
+    // deleteCowData(
+    //     id: bigint
+    // ): Promise<Cow | null>
+}
