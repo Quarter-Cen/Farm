3e23bf6883471409f326b53b6b2f0038ad21624a
TarKhachonsak <khachonsak117@gmail.com>
2025-02-21 16:59:17 +0700

    แก้ไข treatment GETById api

--- src/app/api/veterian/treatment/[id]/route.ts
+++ src/app/api/veterian/treatment/[id]/route.ts
@@ -4,17 +4,21 @@ import { TreatmentService } from "@/services/TreatmentData";
 let treatmentService = new TreatmentService()
 
 //[GET] ดึงข้อมูลการรักษาตาม ID
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, params: { params: { id: string } }) {
     try {
-        const id = BigInt(params.id);
+        const {id} = await params.params;
 
-        const treatment = await treatmentService.getTreatmentByID(id);
+        const treatment = await treatmentService.getTreatmentByID(BigInt(id));
 
         if (!treatment) {
             return NextResponse.json({ error: "Treatment not found" }, { status: 404 });
         }
 
-        return NextResponse.json(treatment, { status: 200 });
+        const jsonResponse = JSON.stringify(treatment, (key, value) =>
+            typeof value === "bigint" ? value.toString() : value
+        );
+
+        return new NextResponse(jsonResponse, { status: 200, headers: { "Content-Type": "application/json" } });
     } catch (error) {
         console.error("Error fetching treatment:", error);
         return NextResponse.json({ error: "Internal server error" }, { status: 500 });
