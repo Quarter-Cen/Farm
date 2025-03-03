c1b775902a70941f98cd43c77b0adadeafea52ae
Kanok <kanoksaengjan@hotmail.com>
2025-03-03 20:38:05 +0700

    fix

--- src/app/(Web_UI)/layout.tsx
+++ src/app/(Web_UI)/layout.tsx
@@ -4,12 +4,10 @@ import Sidebar from "@/components/Sidebar"
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
   return (
     <>  
-        <DefaultGuard>
             <div className="flex min-h-screen scrollbar-custom">
                 <Sidebar />
                 <main className="flex-1 p-6 overflow-auto">{children}</main>
             </div>
-        </DefaultGuard>
     </>
   )
 }
